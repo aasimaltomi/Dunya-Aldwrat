87871d0eac5a937cc5c05ac24cf944a3c89e0e28
Aasim Mohammed Altomi <atomy8774@gmail.com>
2026-09-17 18:55:53 +0300

    feat: add union president name above role

--- js/union-president-message.js
+++ js/union-president-message.js
@@ -3,17 +3,17 @@
     ar:{
       unionPresidentTitle:'كلمة رئيس الاتحاد',
       unionPresidentMessage:'نؤمن في اتحاد شباب الأمة بأن تمكين الشباب يبدأ من تسهيل وصولهم إلى المعرفة والفرص التي تساعدهم على بناء مستقبلهم. ومن هذا المنطلق، جاء مشروع دنيا الدورات ليحوّل كثرة الخيارات إلى تجربة أكثر وضوحًا وتنظيمًا، ويمنح الطالب طريقًا أبسط لاكتشاف المنصات التعليمية المناسبة له. ونطمح أن يكون هذا المشروع خطوة عملية ضمن جهود الاتحاد في دعم الطلاب، وتشجيع التعلم المستمر، وتحويل الأفكار الشبابية إلى مبادرات تخدم المجتمع.',
-      unionPresidentSignature:'رئيس اتحاد شباب الأمة'
+      unionPresidentSignature:'م. بشار محمد الزريقي\nرئيس اتحاد شباب الأمة'
     },
     en:{
       unionPresidentTitle:'Message from the Union President',
       unionPresidentMessage:'At Ummah Youth Union, we believe that empowering young people begins with making knowledge and opportunities easier to access. From this vision, Dunya Al-Dawrat was created to transform the overwhelming number of options into a clearer and more organized experience, helping students discover the learning platforms that best fit their needs. We hope this project will be a practical step within the Union’s efforts to support students, encourage lifelong learning, and turn youth ideas into initiatives that serve the community.',
-      unionPresidentSignature:'President of Ummah Youth Union'
+      unionPresidentSignature:'Eng. Bashar Mohammed Al-Zuraiqi\nPresident of Ummah Youth Union'
     },
     tr:{
       unionPresidentTitle:'Birlik Başkanının Mesajı',
       unionPresidentMessage:'Ümmet Gençleri Birliği olarak, gençleri güçlendirmenin bilgiye ve fırsatlara erişimi kolaylaştırmakla başladığına inanıyoruz. Bu anlayıştan hareketle, Dunya Al-Dawrat projesi çok sayıdaki seçeneği daha açık ve düzenli bir deneyime dönüştürmek, öğrencilerin kendilerine en uygun öğrenme platformlarını daha kolay keşfetmelerine yardımcı olmak amacıyla ortaya çıktı. Bu projenin, birliğimizin öğrencileri destekleme, sürekli öğrenmeyi teşvik etme ve gençlerin fikirlerini topluma hizmet eden girişimlere dönüştürme çabalarının pratik bir adımı olmasını umut ediyoruz.',
-      unionPresidentSignature:'Ümmet Gençleri Birliği Başkanı'
+      unionPresidentSignature:'Müh. Bashar Mohammed Al-Zuraiqi\nÜmmet Gençleri Birliği Başkanı'
     }
   };
 
